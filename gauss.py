ce46f2e85ea9ddd6436b0bbee655e42d17155d83
Betoml5 <betoml5@hotmail.com>
2022-03-28 22:32:15 -0600

    Missing explanation

--- gauss.py
+++ gauss.py
@@ -1,35 +1,74 @@
 # Defining our function as seidel which takes 3 arguments
 # as A matrix, Solution and B matrix
-   
-def seidel(a, x ,b):
-    #Finding length of a(3)       
-    n = len(a)                   
+
+def seidel(a, x, b):
+    # Finding length of a(3)
+    n = len(a)
     # for loop for 3 times as to calculate x, y , z
-    for j in range(0, n):        
+    for j in range(0, n):
         # temp variable d to store b[j]
-        d = b[j]                  
-          
+        d = b[j]
+
         # to calculate respective xi, yi, zi
-        for i in range(0, n):     
+        for i in range(0, n):
             if(j != i):
-                d-=a[j][i] * x[i]
-        # updating the value of our solution        
+                d -= a[j][i] * x[i]
+        # updating the value of our solution
         x[j] = d / a[j][j]
-    # returning our updated solution           
-    return x    
-   
-# int(input())input as number of variable to be solved                
-n = 3                              
-a = []                            
-b = []        
-# initial solution depending on n(here n=3)                     
-x = [0, 0, 0]                        
-a = [[8, 4, -2],[3, 6, -1],[2, -2, 6]]
-b = [24,13,16]
+    # returning our updated solution
+    return x
+
+
+# int(input())input as number of variable to be solved
+n = 3
+a = []
+b = []
+
+dummy_array_1 = []
+dummy_array_2 = []
+dummy_array_3 = []
+
+
+# Valores iniciales
+x = []
+# Valores del final de la matriz
+b = []
+for i in range(3):
+    number = int(input("Ingresa el numero:  " + str(i+1)))
+    dummy_array_1.append(number)
+for i in range(3):
+    number = int(input("Ingresa el numero " + str(i+1)))
+    dummy_array_2.append(number)
+for i in range(3):
+    number = int(input("Ingresa el numero " + str(i+1)))
+    dummy_array_3.append(number)
+
+# Los valores del final de la matriz
+for i in range(3):
+    number = int(input("Ingresa el numero " + str(i+1)))
+    b.append(number)
+
+
+# Valores iniciales
+print("Ingresa los valores iniciales")
+for i in range(3):
+    number = int(input("Ingresa el numero " + str(i+1)))
+    x.append(number)
+
+
+a.append(dummy_array_1)
+a.append(dummy_array_2)
+a.append(dummy_array_3)
+
+
+# initial solution depending on n(here n=3)
+# x = [0, 0, 0]
+# a = [[8, 4, -2], [3, 6, -1], [2, -2, 6]]
+# b = [24, 13, 16]
 print(x)
-  
-#loop run for m times depending on m the error value
-for i in range(0, 25):            
-    x = seidel(a, x, b)
-    #print each time the updated solution
-    print(x)        
+
+# loop run for m times depending on m the error value
+# for i in range(0, 25):
+#     x = seidel(a, x, b)
+#     # print each time the updated solution
+#     print(x)
--- gauss2.py
+++ gauss2.py
@@ -1,10 +1,21 @@
 # Gauss Seidel Iteration
 
+# 20x + y - 2z = 17
+# 3x + 20y -z = -18
+# 2x - 3y + 20z = 25
+
+
+# Formula
+# x = (17-y+2z)/20
+# y = (-18-3x+z)/20
+# z = (25-2x+3y)/20
+
 # Defining equations to be solved
 # in diagonally dominant form
-f1 = lambda x,y,z: (17-y+2*z)/20
-f2 = lambda x,y,z: (-18-3*x+z)/20
-f3 = lambda x,y,z: (25-2*x+3*y)/20
+def f1(x, y, z): return (17-y+2*z)/20
+def f2(x, y, z): return (-18-3*x+z)/20
+def f3(x, y, z): return (25-2*x+3*y)/20
+
 
 # Initial setup
 x0 = 0
@@ -21,19 +32,19 @@ print('\nCount\tx\ty\tz\n')
 condition = True
 
 while condition:
-    x1 = f1(x0,y0,z0)
-    y1 = f2(x1,y0,z0)
-    z1 = f3(x1,y1,z0)
-    print('%d\t%0.4f\t%0.4f\t%0.4f\n' %(count, x1,y1,z1))
-    e1 = abs(x0-x1);
-    e2 = abs(y0-y1);
-    e3 = abs(z0-z1);
-    
+    x1 = f1(x0, y0, z0)
+    y1 = f2(x1, y0, z0)
+    z1 = f3(x1, y1, z0)
+    print('%d\t%0.4f\t%0.4f\t%0.4f\n' % (count, x1, y1, z1))
+    e1 = abs(x0-x1)
+    e2 = abs(y0-y1)
+    e3 = abs(z0-z1)
+
     count += 1
     x0 = x1
     y0 = y1
     z0 = z1
-    
-    condition = e1>e and e2>e and e3>e
 
-print('\nSolution: x=%0.3f, y=%0.3f and z = %0.3f\n'% (x1,y1,z1))
+    condition = e1 > e and e2 > e and e3 > e
+
+print('\nSolution: x=%0.3f, y=%0.3f and z = %0.3f\n' % (x1, y1, z1))
